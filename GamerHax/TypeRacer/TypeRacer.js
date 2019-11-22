1ebd2776ed67f520088b80a1b7d695658a1341b7
ThatFruedDued <57722939+ThatFruedDued@users.noreply.github.com>
2019-11-22 16:51:43 -0500

    Update TypeRacer.js

--- GamerHax/TypeRacer/TypeRacer.js
+++ GamerHax/TypeRacer/TypeRacer.js
@@ -1,2 +1,8 @@
-var answers = document.getElementsByClassName("LQWHetZI bBxVovJm")[0].children;
-var completeFinish = answers[0].innerHTML + answers[1].innerHTML + answers[2].innerHTML + "";
+function hacktivate(){
+  var answers = document.getElementsByClassName("inputPanel")[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0].children;
+  var completeFinish = answers[0].innerHTML + answers[1].innerHTML + answers[2].innerHTML + "";
+  var inputArea = document.getElementsByClassName("txtInput")[0];
+  inputArea.maxLength = 1000;
+  inputArea.value = completeFinish;
+}
+hacktivate();
