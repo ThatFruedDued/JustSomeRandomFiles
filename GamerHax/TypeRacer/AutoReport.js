caf0e1549419dc0da9309cb38efea49fd91b70d8
ThatFruedDued <57722939+ThatFruedDued@users.noreply.github.com>
2019-12-04 10:10:22 -0500

    Update AutoReport.js

--- GamerHax/TypeRacer/AutoReport.js
+++ GamerHax/TypeRacer/AutoReport.js
@@ -1,2 +1,8 @@
 var reportElements = document.getElementsByClassName("infoTable")[0].children[0].children;
-var whySelect= = reportElements[1].children[1]
+var whySelect = reportElements[1].children[1].children[0];
+var reason = reportElements[2].children[1].children[1];
+var subRep = reportElements[3].children[1].children[0];
+
+whySelect.selectedIndex = 2;
+reason.value = "Hi! I'm really the one cheating. I just have to convey this message to you via a report. Please ban me.";
+subRep.click();
